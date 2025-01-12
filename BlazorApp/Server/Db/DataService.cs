f1fab0230f4e8dbb47c2560d4805ee476dc31167
j-uchida <cic44710@gmail.com>
2025-01-12 13:01:31 +0900

    コミット

---
+++ BlazorApp/Server/Db/DataService.cs
@@ -0,0 +1,25 @@
+﻿using BlazorApp.Shared;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+public class DataService
+{
+    private readonly ApplicationDbContext _context;
+
+    public DataService(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public List<Product> GetProducts()
+    {
+        return _context.Products.ToList();
+    }
+
+    public async Task<List<Product>> GetProductsAsync()
+    {
+        return await _context.Products.ToListAsync();
+    }
+}
---
+++ BlazorApp/Shared/Product.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlazorApp.Shared
+{
+    public class Product
+    {
+        public int ProductID { get; set; }
+        public string ProductName { get; set; }
+        public decimal Price { get; set; }
+    }
+
+}
