2344ba1e5087b4d85d6f1f1abfd36e2c25d1cca8
j-uchida <cic44710@gmail.com>
2025-01-12 14:36:16 +0900

    DB接続コード

--- BlazorApp/Server/Controllers/WeatherForecastController.cs
+++ BlazorApp/Server/Controllers/WeatherForecastController.cs
@@ -10,7 +10,6 @@ namespace BlazorApp.Server.Controllers
 {
     [ApiController]
     [Route("[controller]")]
-    [Produces("application/json")]
     public class WeatherForecastController : ControllerBase
     {
         private static readonly string[] Summaries = new[]
